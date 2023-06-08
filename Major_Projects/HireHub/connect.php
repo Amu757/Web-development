f166d7fbd41614b6e82c06f8b37cbaa7bc14d9ec
Aman  Waghmare <93338002+Amu757@users.noreply.github.com>
2023-06-08 14:12:00 +0530

    Add files via upload

---
+++ Major_Projects/HireHub/connect.php
@@ -0,0 +1,8 @@
+<?php
+$fullname = $_POST['name'];
+$m_number = $_POST['number'];
+$account = $_POST['acc_type'];
+$username = $_POST['username'];
+$pass1 = $_POST['pass1'];
+$pass2 = $_POST['pass2'];
+?>
