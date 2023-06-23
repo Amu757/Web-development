329fd267dca2dab2e317142519d7366c9964f11e
Aman  Waghmare <93338002+Amu757@users.noreply.github.com>
2023-06-23 23:38:36 +0530

    Delete Major_Projects/HireHub directory

--- Major_Projects/HireHub/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$fullname = $_POST['name'];
-$m_number = $_POST['number'];
-$account = $_POST['acc_type'];
-$username = $_POST['username'];
-$pass1 = $_POST['pass1'];
-$pass2 = $_POST['pass2'];
-?>
